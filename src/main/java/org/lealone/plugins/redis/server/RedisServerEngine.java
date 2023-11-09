59ebc651a1e37092699b047f329891a573062541
Honghua Zhu <zhh200910@gmail.com>
2023-11-09 08:16:54 +0800

    已经在ProtocolServerEngineBase中统一负责各种api的实现

--- src/main/java/org/lealone/plugins/redis/server/RedisServerEngine.java
+++ src/main/java/org/lealone/plugins/redis/server/RedisServerEngine.java
@@ -5,32 +5,19 @@
  */
 package org.lealone.plugins.redis.server;
 
-import java.util.Map;
-
 import org.lealone.server.ProtocolServer;
 import org.lealone.server.ProtocolServerEngineBase;
 
 public class RedisServerEngine extends ProtocolServerEngineBase {
 
     public static final String NAME = "Redis";
-    private final RedisServer server = new RedisServer();
 
     public RedisServerEngine() {
         super(NAME);
     }
 
     @Override
-    public ProtocolServer getProtocolServer() {
-        return server;
-    }
-
-    @Override
-    public void init(Map<String, String> config) {
-        server.init(config);
-    }
-
-    @Override
-    public void close() {
-        server.stop();
+    protected ProtocolServer createProtocolServer() {
+        return new RedisServer();
     }
 }
